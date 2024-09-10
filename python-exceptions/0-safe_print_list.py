46e32660599d7ca16e31f5e593fe14a3e7f32717
scotty800 <scottyndanga02400@gmail.com>
2024-09-10 16:43:27 +0200

    pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index2 = 0 
+    index2 = 0
     try:
         for index1 in range(x):
             print(my_list[index1], end="")
             index2 += 1
     except IndexError:
-        pass 
+        pass
     print()
     return index2
