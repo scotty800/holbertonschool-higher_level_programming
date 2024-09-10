e21895c78e6ef395c5d99d8f08f51416e6304dba
scotty800 <scottyndanga02400@gmail.com>
2024-09-10 11:09:01 +0200

    Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if 0 <= search < len(my_list):
-        my_list[search] = replace
-        return(my_list)
+    return [replace if x == search else x for x in my_list]
