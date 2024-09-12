f446a58ba97958b9c5b102e3a736943ef87bc7ae
scotty800 <scottyndanga02400@gmail.com>
2024-09-12 12:26:47 +0200

    Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Prints 'My name is' followed by the first and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is' followed by the first and last name.
+
+    Args:
+        first_name (str): The first name of the person.
+        last_name (str, optional): The last name of the person.
+        Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Example:
+        say_my_name("John", "Doe") -> My name is John Doe
+        say_my_name("Jane") -> My name is Jane
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    # Print the name, handling the case where the last name might be empty
+    if last_name:
+        print("My name is {:s} {:s}".format(first_name, last_name))
+    else:
+        print("My name is {:s}".format(first_name))
