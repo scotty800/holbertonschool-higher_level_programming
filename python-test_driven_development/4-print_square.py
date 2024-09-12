03be29bab208c9f2b4099b4d807b36cbf137ccf7
scotty800 <scottyndanga02400@gmail.com>
2024-09-12 17:37:44 +0200

    fix code again

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -23,10 +23,9 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if not isinstance(size, float):
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float):
-        raise TypeError("size must be an integer")
-
     for index in range(size):
         print("#" * size)
