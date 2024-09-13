3ed1604e7ad75d488853ad101f2c8c284e9a2f57
scotty800 <scottyndanga02400@gmail.com>
2024-09-13 16:16:16 +0200

    fix code add new line

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -30,7 +30,7 @@ def text_indentation(text):
     while i < len(text):
         result += text[i]
         if text[i] in ".?:":
-            result += "\n"
+            result += "\n\n"
             i += 1
             while i < len(text) and text[i] == " ":
                 i += 1
