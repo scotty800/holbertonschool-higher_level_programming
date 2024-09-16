bebf398ba7e4ee7a0c0bfba253631a1b20d35e20
scotty800 <scottyndanga02400@gmail.com>
2024-09-16 14:36:46 +0200

    pycodstyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
     """
     Class representing a square with a private size attribute.
     """
-    def __init__(self,size):
+    def __init__(self, size):
         """
         Initializes the Square with a given size.
         :param size: The length of the side of the square.
