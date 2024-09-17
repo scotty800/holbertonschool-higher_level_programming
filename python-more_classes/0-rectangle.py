39d63d6e7b096abf75e6ebbfd8d63f697f1bce25
scotty800 <scottyndanga02400@gmail.com>
2024-09-17 10:20:44 +0200

    Write an empty class Rectangle that defines a rectangle:

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class representing a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Class representing a rectangle.
+    Currently, it is an empty class with no attributes or methods.
+    """
+    pass
