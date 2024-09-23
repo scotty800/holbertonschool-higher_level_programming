f9e46261c8c072b2e73ded836435acb669d86fd7
scotty800 <scottyndanga02400@gmail.com>
2024-09-23 17:36:40 +0200

    Write a class Square that inherits from Rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Validates that the provided value is a positive integer.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Validates that the provided value is a positive integer.
+    """
+    def __init__(self, size):
+        """
+        Validates that the provided value is a positive integer.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+    def area(self):
+        """
+        Validates that the provided value is a positive integer.
+        """
+        return self.__size ** 2
+    
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
