d9a5baab5480cf81a6e89c023fec7565f0ab4801
scotty800 <scottyndanga02400@gmail.com>
2024-09-23 09:25:03 +0200

    pycostyle fic

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,13 +2,13 @@
 """
 Returns the list of available attributes and methods of an object.
 """
+
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
-    
     Args:
         obj: The object to inspect.
-    
     Returns:
         List of strings representing the object's attributes and methods.
     """
