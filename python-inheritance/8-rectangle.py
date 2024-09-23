e85e2ca4441fe86539be5e99bf9ab409769cef89
scotty800 <scottyndanga02400@gmail.com>
2024-09-23 16:41:38 +0200

    delete class and import basegeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,35 +5,7 @@ and an integer_validator method.
 """
 
 
-class BaseGeometry:
-    """
-    Base class for geometry-related operations.
-    """
-
-    def area(self):
-        """
-        Raises an Exception indicating that the area method is not implemented.
-
-        This method is intended to be overridden in a subclass.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that the provided value is a positive integer.
-
-        Args:
-            name (str): The name of the value (for error message purposes).
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -46,5 +18,5 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        __width = width
-        __height = height
+        self.__width = width
+        self.__height = height
