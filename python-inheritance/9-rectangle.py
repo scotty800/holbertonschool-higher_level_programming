d60bf052d4f70cbda6f4425c0714577a9bc77aeb
scotty800 <scottyndanga02400@gmail.com>
2024-09-23 16:51:59 +0200

    Full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Validates that the provided value is a positive integer.
+    """
+    def __init__(self, width, height):
+        """
+        Validates that the provided value is a positive integer.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Validates that the provided value is a positive integer.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Validates that the provided value is a positive integer.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
