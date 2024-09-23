30bbc205bda3673f8356d04dfeb8c6c637cbf8f7
scotty800 <scottyndanga02400@gmail.com>
2024-09-23 09:21:34 +0200

    check PEP 8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-
+"""
+Returns the list of available attributes and methods of an object.
+"""
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    
+    Args:
+        obj: The object to inspect.
+    
+    Returns:
+        List of strings representing the object's attributes and methods.
+    """
     return dir(obj)
