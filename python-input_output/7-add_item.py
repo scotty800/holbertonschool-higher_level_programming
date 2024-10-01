d9badd7ceb98e293cb89d7c9838314a1f081c2df
scotty800 <scottyndanga02400@gmail.com>
2024-10-01 14:49:48 +0200

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Loads an object from a text file containing JSON data.
+"""
+import json
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv[1:]
+filname = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filname)
+except FileNotFoundError:
+    my_list = []
+
+    my_list.extend(args)
+    save_to_json_file(my_list, filname)
