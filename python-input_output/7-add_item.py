6b94a6fa73d008afa57df7e260889ff1b91e6fa1
scotty800 <scottyndanga02400@gmail.com>
2024-10-01 14:54:38 +0200

    add import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,8 @@
 """
 Loads an object from a text file containing JSON data.
 """
-import json
 import sys
-
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
