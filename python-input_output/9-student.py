d88b436fba6558194e9220804dfbbbad4c39e73a
scotty800 <scottyndanga02400@gmail.com>
2024-10-01 15:38:49 +0200

    Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Classe représentant un étudiant.
+"""
+
+
+class student:
+    """
+    Classe représentant un étudiant.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialisation des attributs de l'étudiant.
+        Args:
+        first_name (str): Prénom de l'étudiant.
+        ast_name (str): Nom de famille de l'étudiant.
+        age (int): Âge de l'étudiant.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Renvoie un dictionnaire représentant l'étudiant.
+        Returns:
+        dict: Un dictionnaire contenant les attributs de l'instance.
+        """
+        return self.__dict__
