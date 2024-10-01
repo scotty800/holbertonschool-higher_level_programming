286feb67cbff2f2bb279fa91b97b39aef60d7d62
scotty800 <scottyndanga02400@gmail.com>
2024-10-01 12:20:28 +0200

    Create object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Loads an object from a text file containing JSON data.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Loads an object from a text file containing JSON data.
+    Args:
+    filename (str): The name of the file to load from.
+    """
+    with open(filename, 'r', encoding="utf-8") as fic:
+        json.load(fic)
