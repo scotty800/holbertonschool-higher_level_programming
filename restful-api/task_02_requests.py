690dea569709ccf020846a5f1d202457fad7dd5e
scotty800 <scottyndanga02400@gmail.com>
2024-10-07 17:35:57 +0200

    Consuming and processing data from an API using Python

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    print("Status Code: {}".format(response.status_code))
+
+    if response.status_code == 200:
+        post = response.json()
+        for index in post:
+            print(index['title'])
+
+
+def fetch_and_save_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if response.status_code == 200:
+        post = response.json()
+
+    with open('post.csv', 'w', newline='') as fic:
+        writer = csv.writer(fic)
+        fields = ['id', 'title', 'body']
+        writer.writerow(fields)
+        for index in post:
+            writer.writerow([index['id'], index['title'], index['body']])
