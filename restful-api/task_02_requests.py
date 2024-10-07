720fc49b4cc8c17e12d7ffdbeee8313bb8183b42
scotty800 <scottyndanga02400@gmail.com>
2024-10-07 17:45:31 +0200

    i try to fix with index 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,12 +7,11 @@ import csv
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-
     print("Status Code: {}".format(response.status_code))
 
     if response.status_code == 200:
-        post = response.json()
-        for index in post:
+        posts = response.json()
+        for index in posts:
             print(index['title'])
 
 
@@ -21,11 +20,11 @@ def fetch_and_save_posts():
     response = requests.get(url)
 
     if response.status_code == 200:
-        post = response.json()
-
-    with open('post.csv', 'w', newline='') as fic:
-        writer = csv.writer(fic)
-        fields = ['id', 'title', 'body']
-        writer.writerow(fields)
-        for index in post:
-            writer.writerow([index['id'], index['title'], index['body']])
+        posts = response.json()
+        with open('posts.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            fields = ['id', 'title', 'body']
+            writer.writerow(fields)
+            for index_2 in posts:
+                writer.writerow([index_2['id'], index_2['title'],
+                                 index_2['body']])
