a9b87fc6ec09bfc20bf1c08ed072510221e951b7
scotty800 <scottyndanga02400@gmail.com>
2024-10-09 13:00:01 +0200

    Develop a Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28,
+             "city": "Los Angeles"},
+}
+
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data')
+def data():
+    return jsonify(users)
+
+
+@app.route('/status')
+def status():
+    return "OK"
+
+
+@app.route('/users/<username>')
+def name(username):
+    if username in users:
+        return jsonify(users[username])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add():
+    data = request.get_json()
+
+    username = data.get('username')
+    name = data.get('name')
+    age = data.get('age')
+    city = data.get('city')
+
+    if username in users:
+        return jsonify({"message": "User already exists"}), 400
+
+    users[username] = {
+        'name': name,
+        'age': age,
+        'city': city
+    }
+
+    return jsonify({
+        "message": "User added successfully",
+        "user": users[username]
+    }), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
