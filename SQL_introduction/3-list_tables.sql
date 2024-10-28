1d260475d089cd9e209a6615efa1fb1874f5babb
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 16:24:54 +0100

    List tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Cette commande affiche la liste de toutes les tables présentes dans la base de données sélectionnée.
+-- Elle est utile pour vérifier les tables disponibles et s'assurer de la structure de la base de données active.
+SHOW TABLES;
