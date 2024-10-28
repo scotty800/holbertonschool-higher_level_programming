3c8c2cf7ddcb1dce7a1be67da12cad6a755d4ca9
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 23:05:15 +0100

    4. First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Cette commande crée une nouvelle table nommée "first_table" avec deux colonnes :
+-- "id" de type entier (INT) et "name" de type chaîne de caractères (VARCHAR) pouvant contenir jusqu'à 256 caractères.
+CREATE TABLE first_table (id INT, name VARCHAR(256));
