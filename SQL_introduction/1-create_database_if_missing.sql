9e327731c89500d73a8838b290da2086fab5dd64
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 15:41:07 +0100

    Create a database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Cette commande crée une base de données nommée "hbtn_0c_0" si elle n'existe pas déjà.
+-- Le mot-clé "IF NOT EXISTS" permet d'éviter les erreurs si la base de données est déjà présente sur le serveur.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
