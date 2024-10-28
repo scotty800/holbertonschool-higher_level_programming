8b40d13e1528b7b3dd2daa48d848be693aafee1c
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 15:17:49 +0100

    List databases

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Cette commande affiche la liste de toutes les bases de données disponibles sur le serveur MySQL.
+-- Elle est utile pour vérifier quelles bases de données existent et pour voir les bases auxquelles l'utilisateur actuel a accès.
+SHOW DATABASES;
