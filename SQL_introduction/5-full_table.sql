840b8650810b949d1f03b6715c1a5793c975d117
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 23:18:40 +0100

    5. Full description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Cette commande affiche la requête SQL complète utilisée pour créer la table "first_table".
+-- Elle permet de visualiser la structure exacte de la table, y compris les types de données, les contraintes, 
+-- et les options de création, comme si on la recréait à partir de zéro.
+SHOW CREATE TABLE first_table;
