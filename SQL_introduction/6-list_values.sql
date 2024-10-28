13667a0e4fc9d59ef68ccf54ff13eb68bb05903d
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 23:25:34 +0100

    List all in table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Cette commande sélectionne et affiche toutes les colonnes et toutes les lignes de la table "first_table".
+-- Elle est utile pour visualiser l'ensemble des données actuellement stockées dans la table.
+SELECT * FROM first_table;
