6df5bfe88667525359b56717778a0815983f830e
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 23:51:39 +0100

    Count 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Cette commande compte le nombre de lignes dans la table "first_table" où la valeur de "id" est égale à 89.
+-- Elle est utile pour vérifier si une ou plusieurs entrées avec "id = 89" existent dans la table.
+SELECT COUNT(*) FROM first_table where id = 89;
