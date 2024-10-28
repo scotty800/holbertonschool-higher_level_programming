330346bdc6a6785934eb9b2d9c7716971c26fe76
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 00:12:29 +0100

    ADD review

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Création de la table "second_table" avec trois colonnes :
 -- "id" de type entier (INT), "name" de type chaîne de caractères (VARCHAR) pouvant contenir jusqu'à 256 caractères,
 -- et "score" de type entier (INT) pour stocker des scores.
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 -- Insertion de plusieurs lignes dans la table "second_table" avec des valeurs spécifiques pour chaque colonne :
 -- "id" identifiant chaque ligne, "name" pour le nom de la personne, et "score" pour le score associé.
 INSERT INTO second_table(id, name, score)
