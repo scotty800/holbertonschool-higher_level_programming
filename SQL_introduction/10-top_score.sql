90aded778df6d1fc8a83efbfe5154b4eeaa7c26a
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 00:19:35 +0100

    List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Cette commande sélectionne les colonnes "score" et "name" de la table "TABLE_NAME" 
+-- et affiche les résultats triés par "score" de manière décroissante (du plus élevé au plus bas).
+-- Elle est utile pour obtenir une liste des scores en ordre décroissant, en associant chaque score à son nom correspondant.
+SELECT score, name FROM TABLE_NAME ORDER BY score DESC;
