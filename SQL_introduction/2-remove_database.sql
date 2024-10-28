cb9654d41d352a34fa06ed64f46ca70c7c4ed728
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 16:03:25 +0100

    modifi

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,1 +1,3 @@
+-- Cette commande supprime la base de données "hbtn_0c_0" si elle existe déjà sur le serveur.
+-- Le mot-clé "IF EXISTS" permet d'éviter une erreur si la base de données n'est pas présente.
 DROP DATABASE IF EXISTS hbtn_0c_0;
