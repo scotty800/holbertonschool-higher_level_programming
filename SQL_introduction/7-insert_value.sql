3d716d603a4af36529108d1b933a2378fa1740bc
scotty800 <scottyndanga02400@gmail.com>
2024-10-28 23:37:07 +0100

    add review

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,1 +1,4 @@
+-- Cette commande insère une nouvelle ligne dans la table "first_table" avec les valeurs suivantes :
+-- "id" défini à 18 et "name" défini à 'Best School'.
+-- Elle permet d'ajouter une nouvelle entrée avec ces données spécifiques dans la table.
 INSERT INTO first_table (id, name) VALUES (18, 'Best School')
