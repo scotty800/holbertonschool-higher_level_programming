80b21687d84a1ee6dc9cb3431e41a153dce6f5e1
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 15:23:40 +0100

    Always a name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Cette commande crée une table nommée "force_name" avec deux colonnes :
+-- "id" de type entier (INT), servant de clé ou identifiant unique pour chaque enregistrement,
+-- et "name" de type chaîne de caractères (VARCHAR) pouvant contenir jusqu'à 256 caractères.
+CREATE TABLE force_name (id INT, name VARCHAR(256));
