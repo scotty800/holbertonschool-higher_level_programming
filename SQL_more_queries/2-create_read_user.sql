d53e3d07e225e2bff6b1a0923607bc75e69eda7f
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 15:13:05 +0100

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Cette commande crée un utilisateur nommé "user_0d_2" avec l'hôte "localhost" et un mot de passe "user_0d_2_pwd".
 -- Le mot-clé "IF NOT EXISTS" permet de s'assurer que l'utilisateur n'est créé que s'il n'existe pas déjà.
-CREATE USER ID NOT EXISTS  'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS  'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Cette commande accorde uniquement le privilège SELECT (lecture) sur toutes les bases de données et tables (*.*)
 -- à l'utilisateur "user_0d_2" lorsqu'il se connecte depuis "localhost".
-GRANT SELECT ON *.* TO `user_0d_2`@`localhost`;
+GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`;
