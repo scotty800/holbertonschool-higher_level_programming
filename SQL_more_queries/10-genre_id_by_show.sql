75487e7f3440ddb6a24ea9b5c2b16cca44e757e7
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 18:28:31 +0100

    review

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,4 @@
 -- Cette commande sélectionne le titre des émissions de télévision
--- à partir de la table "tv_shows" et l'identifiant de genre 
--- à partir de la table "tv_show_genres".
--- Elle utilise une jointure (JOIN) pour associer les émissions de télévision 
--- à leurs genres respectifs en reliant "tv_shows.id" à "tv_show_genres.tv_show_id".
--- Les résultats sont triés par le titre des émissions de télévision (tv_shows.title) 
--- en ordre croissant, puis par l'identifiant de genre (tv_show_genres.genre_id) en ordre croissant.
 SELECT tv_shows.title, tv_shows_genres.genre.id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
