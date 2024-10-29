ca7011332721260f67986e31165f4f9bed7b0fb0
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 10:11:55 +0100

    Score too low

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Cette commande supprime de la table "second_table" tous les enregistrements
+-- où la valeur de "score" est inférieure ou égale à 5.
+-- Elle est utilisée pour enlever les enregistrements ayant un score faible (5 ou moins).
+DELETE FROM second_table WHERE score <= 5;
