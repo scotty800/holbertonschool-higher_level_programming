4cc9f403e57101fffb98095289a714d715276ad0
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 18:29:30 +0100

    change code

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Cette commande sélectionne le titre des émissions de télévision
-SELECT tv_shows.title, tv_shows_genres.genre.id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
