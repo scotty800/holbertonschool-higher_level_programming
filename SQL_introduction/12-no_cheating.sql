5fadb9ec8aea28f42624d7495a70a5a5b2f10b10
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 09:58:23 +0100

    Cheating is bad

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Cette commande met à jour la table "second_table" en modifiant la valeur de "score" à 10
+-- pour l'enregistrement où la colonne "name" est égale à 'Bob'.
+-- Elle permet de changer le score de Bob à une valeur fixe de 10.
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
