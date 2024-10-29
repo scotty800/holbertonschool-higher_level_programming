e71bf677f0e655f61214748f5c0cd66e8c7445b4
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 14:24:30 +0100

    My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Cette commande affiche les privilèges accordés à l'utilisateur "user_0d_1" lorsqu'il se connecte depuis "localhost".
+-- Elle est utile pour vérifier les permissions spécifiques de cet utilisateur sur la base de données.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Cette commande affiche les privilèges accordés à l'utilisateur "user_0d_2" lorsqu'il se connecte depuis "localhost".
+-- Comme pour le premier utilisateur, elle permet de vérifier et de réviser les autorisations données à cet utilisateur.
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
