4f6d006de8e27c19d5b365f3e9404955a279106f
scotty800 <scottyndanga02400@gmail.com>
2024-10-29 10:27:39 +0100

    Average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Cette commande calcule la moyenne des scores dans la table "second_table".
+-- La fonction AVG(score) retourne la moyenne des valeurs de la colonne "score".
+-- Le résultat est affiché avec l'alias "average" pour clarifier la sortie.
+SELECT AVG(score) AS average FROM second_table;
