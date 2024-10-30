9d915a43537bd8839d12f435338ec8260584eec8
scotty800 <scottyndanga02400@gmail.com>
2024-10-30 16:20:01 +0100

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Connexion à la base de données MySQL
+avec les informations
+d'identification passées en arguments
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Connexion à la base de données
+    MySQL avec les informations
+    d'identification passées en arguments
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = db.cursor()
+    """Création d'un objet curseur pour exécuter les commandes SQL"""
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    """Récupération de toutes les lignes de résultat de la requête"""
+    state = cursor.fetchall()
+
+    for row in state:
+        print(row)
+
+    cursor.close()
+    db.close()
