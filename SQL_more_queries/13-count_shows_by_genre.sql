45f83450fae9add128a823a16d7bbc808800edd7
scotty800 <scottyndanga02400@gmail.com>
2024-10-30 10:39:10 +0100

    add review

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Cette commande sélectionne le titre des émissions de télévision
-SELECT genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
