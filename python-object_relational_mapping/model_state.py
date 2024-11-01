d1113c30af19e349b5ae21a3d3edbff94620d2ad
scotty800 <scottyndanga02400@gmail.com>
2024-11-01 13:37:40 +0100

    pyvostyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,12 +2,15 @@
 
 """Class states and instance of base = declarative_base"""
 
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class SomeClass(Base):
+
+class SomeStates(Base):
+    """Class States"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
