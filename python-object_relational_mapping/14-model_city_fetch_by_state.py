d85a69994b70d7bddbc9874e881a79df5cb0409f
scotty800 <scottyndanga02400@gmail.com>
2024-11-01 16:46:15 +0100

    chmod

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 
-""" lists all State objects from the database hbtn_0e_6_usa"""
+"""prints all City objects from the database hbtn_0e_14_usa"""
 
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+import sys
 
 
 if __name__ == "__main__":
 
-    """Connect to the database and get the states"""
+    """Connect to the database"""
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
-                           (mysql_username,  mysql_password, database_name))
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(City).join(State).order_by(City.id).all()
 
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    query = session.query(City, State).join(State)
+
+    for city, state in query.all():
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
 
     session.commit()
     session.close()
